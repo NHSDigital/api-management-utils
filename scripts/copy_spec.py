e90248544637e952148dee32117ac651fefdb66d
raghuram grandhi <raghu.ramgrandhi1@nhs.net>
2025-12-08 15:15:53 +0000

    s3 bucket name updated.

--- scripts/copy_spec.py
+++ scripts/copy_spec.py
@@ -20,21 +20,19 @@ def upload_to_s3(file_path: Path, bucket_name: str, folder_name: str):
         sys.exit(1)
 
 
-def main(bucket_name: str, repo_name: str, working_directory:str):
-    
+def main(bucket_name: str, repo_name: str):
     cwd = os.getcwd()
     print("Current working directory:", cwd)
 
-    # Go up one level to reach utils/
-    #json_dir = os.path.dirname(os.path.dirname(os.getcwd()))
-    #json_dir = os.chdir(working_directory)
-    #print("Utils directory:", json_dir)
-
-    root_dir = Path.cwd().parents[1]     # go up 2 levels
+    root_dir = Path.cwd().parents[1]
     json_file = root_dir / f"{repo_name}.json"
     minified_json = "spec.json"
 
     print(json_file)
+
+    if not json_file.is_file():
+        print(f"[ERROR] JSON spec not found: {json_file}")
+        return 1
 
     with open(json_file, "r") as f:
         data = json.load(f)
@@ -42,29 +40,21 @@ def main(bucket_name: str, repo_name: str, working_directory:str):
     with open(minified_json, "w") as f:
         json.dump(data, f, separators=(",", ":"))
 
-    #json_file = f"{repo_name}.json"
-
-    # Build path to JSON file
-    #json_path = os.path.join(json_dir, json_file)
-
     upload_to_s3(minified_json, bucket_name, repo_name)
 
     print("[DONE] Processing complete.")
     return 0
 
-
 if __name__ == "__main__":
     print("Hitting main")
 
-    if len(sys.argv) != 4:
-        print("Usage: python copy_spec_to_s3.py <s3_bucket_name> <repo_name> <Working Directory>")
+    if len(sys.argv) != 3:
+        print("Usage: python copy_spec_to_s3.py <s3_bucket_name> <repo_name>")
         sys.exit(1)
 
     bucket_name = sys.argv[1]
     repo_name = sys.argv[2]
-    working_directory = sys.argv[3]
     print(f"Repo name: {repo_name}")
     print(f"Bucket name: {bucket_name}")
-    print(f"Working Directory: {working_directory}")
 
-    sys.exit(main(bucket_name,repo_name,working_directory))
+    sys.exit(main(bucket_name,repo_name))
