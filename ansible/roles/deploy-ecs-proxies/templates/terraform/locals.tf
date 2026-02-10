929edadd7320ae2188dba47739b103217d38f34e
CLJ2006 <clarissa.john2@nhs.net>
2026-02-10 21:43:57 +0000

    retag changes

--- ansible/roles/deploy-ecs-proxies/templates/terraform/locals.tf
+++ ansible/roles/deploy-ecs-proxies/templates/terraform/locals.tf
@@ -43,18 +43,41 @@ locals {
 
   }
 
-  ecs_service = [
-  {% for container in ecs_service %}
-    {{
-        (
-          container
-          | combine(
-            {'image': '${local.account_id}.dkr.ecr.eu-west-2.amazonaws.com/' + service_id + '_' + container.name + ':' + build_label }
+ecs_service = [
+{% for container in ecs_service %}
+
+  # DEBUG: print the boolean value Jinja sees
+  {% if use_ecs_tag %}
+    {% set _ = print("DEBUG: use_ecs_tag = TRUE") %}
+  {% else %}
+    {% set _ = print("DEBUG: use_ecs_tag = FALSE") %}
+  {% endif %}
+
+  {% set image_tag = (
+        '${local.account_id}.dkr.ecr.eu-west-2.amazonaws.com/'
+        + service_id + '_' + container.name
+        + (
+            ":ecs-" + build_label
+            if use_ecs_tag and container.name == "canary-api"
+            else ":" + build_label
           )
-        ) | to_json
-    }},
-  {% endfor %}
-  ]
+    )
+  %}
+
+
+  {{
+      (
+        container
+        | combine(
+          {
+            'image': image_tag
+          }
+        )
+      ) | to_json
+  }},
+
+{% endfor %}
+]
 
   exposed_service = element(matchkeys(local.ecs_service, local.ecs_service.*.expose, list(true)), 0)
 
