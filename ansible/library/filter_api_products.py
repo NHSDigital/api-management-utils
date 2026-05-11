a4eb8239de3b84e492c17f0bc9cdaa54c9eae0b9
Sathiyakumar Ganesan <sathiyakumar.ganesan1@nhs.net>
2026-05-11 14:16:05 +0100

    Refactor code for improved readability and consistency in update_schema.py, ecs_service.py, and filter_api_products.py

--- ansible/library/filter_api_products.py
+++ ansible/library/filter_api_products.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python
 
-from ansible.module_utils.basic import *
+from ansible.module_utils.basic import AnsibleModule
 import json
+
 
 def main():
     fields = {
         "products": {"default": True, "type": "str"},
-        "environment": {"default": True, "type": "str"}
+        "environment": {"default": True, "type": "str"},
     }
 
     module = AnsibleModule(argument_spec=fields)
 
-    products_string = module.params['products']
-    json_acceptable_string = products_string.replace("'", "\"")
+    products_string = module.params["products"]
+    json_acceptable_string = products_string.replace("'", '"')
     products = json.loads(json_acceptable_string)
     current_env = module.params["environment"]
 
     for product in products:
-        filtered_product = list(filter(lambda env: env == current_env, product["environments"]))
+        filtered_product = list(
+            filter(lambda env: env == current_env, product["environments"])
+        )
         product["environments"] = filtered_product
 
     module.params.update({"products": products})
-    
+
     module.exit_json(changed=True, meta=module.params)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
